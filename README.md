d5710178798f45eff54018cb8974087fde94cb6c
Ryu114-Sato <ryuichi114@icloud.com>
2025-10-12 01:16:32 +0900

    fix purchase.

--- README.md
+++ README.md
@@ -79,3 +79,23 @@ Check out the [deployment documentation](https://nuxt.com/docs/getting-started/d
 
 ### npx nuxt dev 
  起動
+
+
+##
+### git init
+現在のディレクトリで新しい Git リポジトリを初期化します。 （これにより、バージョン管理が開始されます。）
+### git add README.md
+README.md ファイルをステージングエリアに追加します。 （全ファイルを追加する場合は git add --all とします。）
+### git commit -m "first commit"
+ステージングエリアにある変更を「first commit」というメッセージでコミットします。 （最初の変更履歴を記録します。）
+### git branch -M main
+現在のブランチの名前を強制的に「main」に変更します。 （ローカルリポジトリのデフォルトブランチを「main」に設定します。）
+### git remote add origin https://github.com/Ryu114-Sato/Nuxt-Project-1.git
+GitHub 上のリポジトリ URL を「origin」という名前でリモートリポジトリとして登録します。 （ローカルリポジトリとリモートリポジトリを連携させるための設定です。）
+
+### git push -u origin main --force
+最初にgithubにpushする時のみforce コマンドでmainブランチを上書きする
+
+### git push -u origin feature_202504
+ローカルの「main」ブランチをリモートの「origin」リポジトリにプッシュし、今後のプッシュ・プルの対象として追跡関係を設定します。 （これにより、リモートにも初回のコミット内容が反映されます。）
+### git branch feature_202504
--- app/pages/purchase.vue
+++ app/pages/purchase.vue
@@ -2,63 +2,65 @@
     <div>
         <h1>ご購入手続き</h1>
         <h2>配送先情報</h2>
-        <div>
+        <form @submit.prevent="validate">
             <div>
-                ※配送先住所に誤りがある場合は、住所不明のため配送不可となります。送付先ご住所。郵便番号に誤り
-                がないか十分にご確認をお願いいたします。
+                <div>
+                    ※配送先住所に誤りがある場合は、住所不明のため配送不可となります。送付先ご住所。郵便番号に誤り
+                    がないか十分にご確認をお願いいたします。
+                </div>
+                <div>
+                    氏名 必須
+                    <p><input type="text" placeholder="性" v-model="userInfo.shimei" /><input type="text" placeholder="名" v-model="userInfo.miyoji" /></p>
+                    電話番号 必須
+                    <p><input type="number" placeholder="08012345678" v-model="userInfo.phoneNumber" /></p>
+                    郵便番号 必須
+                    <p><input type="number" placeholder="00000000" /></p>
+                    
+                    <p>都道府県 必須</p>
+                        <label for="pref">都道府県を選択：</label>
+                        <select v-model="selectedPlace" id="pref">
+                        <option disabled value="">選択してください</option>
+                        <option
+                            v-for="(name, code) in todoData"
+                            :key="code"
+                            :value="code"
+                        >
+                            {{ name }}
+                        </option>
+                        </select>
+
+                        <p>選択された都道府県コード: {{ selectedPlace }}</p>
+                        <p v-if="todoData">選択された都道府県名: {{ (todoData[selectedPlace] || '') }}</p>
+                    番地 必須
+                    <p><input type="text" /></p>
+
+                    建物名・部屋番号
+                    <p><input type="text" /></p>
+                </div>
             </div>
             <div>
-                氏名 必須
-                <p><input type="text" placeholder="性" /><input type="text" placeholder="名" /></p>
-                電話番号 必須
-                <p><input type="number" placeholder="08012345678" /></p>
-                郵便番号 必須
-                <p><input type="number" placeholder="00000000" /></p>
-                
-                <p>都道府県 必須</p>
-                    <label for="pref">都道府県を選択：</label>
-                    <select v-model="selectedPlace" id="pref">
-                    <option disabled value="">選択してください</option>
-                    <option
-                        v-for="(name, code) in todoData"
-                        :key="code"
-                        :value="code"
-                    >
-                        {{ name }}
-                    </option>
-                    </select>
-
-                    <p>選択された都道府県コード: {{ selectedPlace }}</p>
-                    <p v-if="todoData">選択された都道府県名: {{ (todoData[selectedPlace] || '') }}</p>
-                番地 必須
-                <p><input type="text" /></p>
-
-                建物名・部屋番号
-                <p><input type="text" /></p>
+                <h2>お支払い方法</h2>
+                <input type="radio" id="option1" v-model="radio" value="クレジットカード" />
+                <label for="クレジットカード">クレジットカード</label>
+                <br />
+                <input type="radio" id="option2" v-model="radio" value="コンビニ決済" />
+                <label for="コンビニ決済">コンビニ決済</label>
+                <br />
+                <input type="radio" id="option3" v-model="radio" value="キャリア決済" />
+                <label for="キャリア決済">キャリア決済</label>
+                <br />
+                <p>あなたが選択した項目：{{ radio }}</p>
             </div>
-        </div>
-        <div>
-            <h2>お支払い方法</h2>
-            <input type="radio" id="option1" v-model="radio" value="クレジットカード" />
-            <label for="クレジットカード">クレジットカード</label>
-            <br />
-            <input type="radio" id="option2" v-model="radio" value="コンビニ決済" />
-            <label for="コンビニ決済">コンビニ決済</label>
-            <br />
-            <input type="radio" id="option3" v-model="radio" value="キャリア決済" />
-            <label for="キャリア決済">キャリア決済</label>
-            <br />
-            <p>あなたが選択した項目：{{ radio }}</p>
-        </div>
-        <div>
-            <p><button @click="goPurchaseDetails()" >次へ進む</button></p>
-            <p><button @click="$router.push('/')">戻る</button></p>
-        </div>
+            <div>
+                <p><button @click="goPurchaseDetails()" >次へ進む</button></p>
+                <p><button @click="$router.push('/')">戻る</button></p>
+            </div>
+        </form>
     </div>
 </template>
 
 <script setup lang="ts" >
-import {ref, reactive} from "vue"
+import {ref, reactive, watch} from "vue"
 import axios from 'axios'
 
 /* 
@@ -86,18 +88,6 @@ import axios from 'axios'
 /* Storeに保存して遷移先で取得する、戻る押下時に何か値を保存していた場合は再度反映する */
 const radio = ref("クレジットカード")
 const selectedPlace = ref("")
-const places = reactive([
-    {
-        id: 1, name:"Tokyo"
-    },
-    {
-        id: 2, name:"Osaka"
-    },
-    {
-        id: 3, name: "Kyoto"
-    }
-])
-
 /* 
 API取得して表示するのか? > 都道府県のデータはNuxt Contentで静的データを読み込むのか？ 　
 */
@@ -112,6 +102,18 @@ axios
 
   console.log("todata is:"+JSON.stringify(todoData.value))
 
+const userInfo = reactive({
+    shimei :"",
+    miyoji :"",
+    phoneNumber:""
+})
+
+watch(userInfo, (val) => {
+  if (!val) return;
+  console.log(`userInfo:${JSON.stringify(userInfo)}`)
+  //setTimeout(() => (userInfo.value = false), 2000);
+});
+console.log("userinfo")
 
 
 
